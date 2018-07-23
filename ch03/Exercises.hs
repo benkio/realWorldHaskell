e15f2a85e75327d4bb233b1098f31eda35ab708c
Enrico Benini <ebenini@mdsol.com>
2018-07-23 18:03:34 +0100

    Add some exercises, do calculate turn

--- ch03/Exercises.hs
+++ ch03/Exercises.hs
@@ -1,6 +1,11 @@
 import Data.Ord
 import Data.List
 
+data Tree a = Node a (Tree a) (Tree a)
+            | Empty
+            deriving (Show)
+
+
 myLength :: [a] -> Int
 myLength [] = 0
 myLength xs = internalCount 0 xs
@@ -23,3 +28,22 @@ myIsPalindrome xs
 
 mySortListOfList :: [[a]] -> [[a]]
 mySortListOfList xxs = sortBy (\xs ys -> (length xs) `compare` (length ys)) xxs
+
+myIntersperse :: a -> [[a]] -> [a]
+myIntersperse _ [] = []
+myIntersperse _ (x:[]) = x
+myIntersperse x (ys:yys) = (ys++[x]) ++ (myIntersperse x yys)
+
+treeHopCount :: Tree a -> Int
+treeHopCount Empty = 0
+treeHopCount (Node x r l) = 1 + (max (treeHopCount r) (treeHopCount l) )
+
+Data Direction = Left
+               | Right
+               | Straight
+               deriving (Show)
+
+type 2DPoint = (Int, Int)
+
+calculateTurn :: 2DPoint -> 2DPoint -> 2DPoint -> Direction
+calculateTurn (x1, y1) (x2, y2) (x3, y3) =
