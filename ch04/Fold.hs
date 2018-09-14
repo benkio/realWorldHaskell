d44d9e36ef1d3b773a3f0594ea86a6b5a9c275ff
Enrico Benini <ebenini@mdsol.com>
2018-09-14 17:25:52 +0100

    Filter with fold right. Next, map with fold right

--- ch04/Fold.hs
+++ ch04/Fold.hs
@@ -1,4 +1,17 @@
 
-myFoldl :: [a] -> b -> (a -> b -> b) -> b
-myFoldl (x:xs) z f = myFoldl xs (f x z) f
+myFoldl :: [a] -> b -> (b -> a -> b) -> b
+myFoldl (x:xs) z f = myFoldl xs (f z x) f
 myFoldl [] z _ = z
+
+myFoldr :: [a] -> b -> (a -> b -> b) -> b
+myFoldr [] z _ = z
+myFoldr (x:xs) z f = f x (myFoldr xs z f)
+
+myFilter :: (a -> Bool) -> [a] -> [a]
+myFilter p []   = []
+myFilter p (x:xs)
+    | p x       = x : myFilter p xs
+    | otherwise = myFilter p xs
+
+myFilter' :: (a -> Bool) -> [a] -> [a]
+myFilter' p xs = myFoldr xs [] (\x acc -> if p x then (acc ++ [x]) else acc)
--- ch04/Sum.hs
+++ ch04/Sum.hs
@@ -2,3 +2,6 @@
 mySum xs = helper 0 xs
            where helper acc (x:xs) = helper (acc + x) xs
                  helper acc [] = acc
+
+
+mySum' xs = foldl (+) 0
