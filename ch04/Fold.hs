ee25b459999a210e26c1080884007c4e9d1e09c3
Enrico Benini <ebenini@mdsol.com>
2018-09-20 15:24:30 +0100

    Finished the Fold section. Execises.

--- ch04/Fold.hs
+++ ch04/Fold.hs
@@ -15,3 +15,23 @@ myFilter p (x:xs)
 
 myFilter' :: (a -> Bool) -> [a] -> [a]
 myFilter' p xs = myFoldr xs [] (\x acc -> if p x then (acc ++ [x]) else acc)
+
+myMap :: [a] -> (a -> b) -> [b]
+myMap xs f = foldr (\a b -> f a : b) [] xs
+
+myFoldl' :: (b -> a -> b) -> b -> [a] -> b
+myFoldl' f z xs = foldr step id xs z
+  where step x g a = g (f a x)
+
+identity :: [a] -> [a]
+identity xs = foldr (:) [] xs
+
+append :: [a] -> [a] -> [a]
+append xs ys = foldr (:) ys xs
+
+-- foldl evaluation, all the computation is stored
+-- foldl (+) 0 (1:2:3:[])
+--           == foldl (+) (0 + 1)             (2:3:[])
+--           == foldl (+) ((0 + 1) + 2)       (3:[])
+--           == foldl (+) (((0 + 1) + 2) + 3) []
+--           ==           (((0 + 1) + 2) + 3)
