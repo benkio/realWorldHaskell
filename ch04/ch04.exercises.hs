174f085ccea9d83b5fd1ea935708215044f71b6b
Enrico <benkio89@gmail.com>
2018-09-24 01:51:44 +0200

    Add any, cycle and myWords

--- ch04/ch04.exercises.hs
+++ ch04/ch04.exercises.hs
@@ -1,3 +1,16 @@
 myAny :: (a -> Bool) -> [a] -> Bool
 myAny f [] = False
 myAny f xs = foldl (\b a -> f a || b) False xs
+
+myCycle :: [a] -> [a]
+myCycle xs = foldr (:) (myCycle xs) xs
+
+myWords :: String -> [String]
+myWords phrase = filter (/= "") $ foldl step [""] phrase
+  where
+    step :: [[Char]] -> Char -> [[Char]]
+    step acc c
+      | (last acc) == "" && (c == ' ') = acc
+      | c == ' ' || c == '\n' = acc ++ [""]
+      | last acc == "" = acc ++ [""++[c]]
+      | otherwise = (init acc) ++ [(last acc) ++ (""++[c])]
