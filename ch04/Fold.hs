43e42ef8a60ebc946c44a841b8aff9a11e2b2033
Enrico Benini <ebenini@mdsol.com>
2018-10-10 17:10:04 +0100

    Finish chapter 4

--- ch04/Fold.hs
+++ ch04/Fold.hs
@@ -35,3 +35,31 @@ append xs ys = foldr (:) ys xs
 --           == foldl (+) ((0 + 1) + 2)       (3:[])
 --           == foldl (+) (((0 + 1) + 2) + 3) []
 --           ==           (((0 + 1) + 2) + 3)
+
+foldl' _ zero [] = zero
+foldl' step zero (x:xs) =
+  let new = step zero x
+  in new `seq` foldl' step new xs
+
+-- incorrect: seq is hidden by the application of someFunc
+-- since someFunc will be evaluated first, seq may occur too late
+-- hiddenInside x y = someFunc (x `seq` y)
+
+-- incorrect: a variation of the above mistake
+-- hiddenByLet x y z = let a = x `seq` someFunc y
+--                     in anotherFunc a z
+
+-- -- correct: seq will be evaluated first, forcing evaluation of x
+-- onTheOutside x y = x `seq` someFunc y
+
+-- evaluates x and y first.
+-- chained x y z = x `seq` y `seq` someFunc z
+
+badExpression step zero (x:xs) =
+  seq (step zero x)
+      (badExpression step (step zero x) xs)
+
+strictPair (a,b) = a `seq` b `seq` (a,b)
+
+strictList (x:xs) = x `seq` x : strictList xs
+strictList []     = []
