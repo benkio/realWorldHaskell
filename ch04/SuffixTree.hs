5f331b34f5f4ad2a3f7e56fb83f992fb97ae48d5
Enrico Benini <ebenini@mdsol.com>
2018-10-10 16:50:43 +0100

    ch 4 - Space leaks and strict evaluation

--- ch04/SuffixTree.hs
+++ ch04/SuffixTree.hs
@@ -1,3 +1,5 @@
+import Data.ByteString (tails, ByteString)
+
 suffixes :: [a] -> [[a]]
 suffixes xs@(_:xs') = xs :suffixes xs'
 suffixes _ = []
@@ -5,3 +7,18 @@ suffixes _ = []
 noAsPattern :: [a] -> [[a]]
 noAsPattern (x:xs) = (x:xs) :suffixes xs
 noAsPattern _ = []
+
+suffixes2 :: ByteString -> [ByteString]
+suffixes2 xs = init (tails xs)
+
+compose :: (b -> d) -> (a -> b) -> a -> d
+compose f g a = f (g a)
+
+suffixes3 :: ByteString -> [ByteString]
+suffixes3 xs = compose init tails xs
+
+suffixes4 :: ByteString -> [ByteString]
+suffixes4 = compose init tails
+
+suffixes5 :: ByteString -> [ByteString]
+suffixes5 = init . tails
