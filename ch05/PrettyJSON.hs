426517bdb106d5beb33022e3be1156cb2979e23d
Enrico Benini <ebenini@mdsol.com>
2018-11-07 16:22:54 +0000

    Chapter 5: Arrays and objects, and the module header

--- ch05/PrettyJSON.hs
+++ ch05/PrettyJSON.hs
@@ -2,6 +2,10 @@ module PrettyJSON where
 
 import PrettyStub
 import SimpleJSON
+import Prelude hiding ((<>))
+import Numeric
+import Data.Bits
+import Data.Char
 
 renderJValue :: JValue -> Doc
 renderJValue (JBool True) = text "true"
@@ -9,3 +13,36 @@ renderJValue (JBool False) = text "false"
 renderJValue JNull = text "null"
 renderJValue (JNumber num) = double num
 renderJValue (JString str) = string str
+
+enclose :: Char -> Char -> Doc -> Doc
+enclose rightChar leftChar doc = char leftChar <> doc <> char rightChar
+
+string :: String -> Doc
+string = enclose '"' '"' . hcat . map oneChar
+
+oneChar :: Char -> Doc
+oneChar c = case lookup c simpleEscapes of
+              Just r -> text r
+              Nothing | mustEscape c -> hexEscape c
+                      | otherwise    -> char c
+    where mustEscape c = c < ' ' || c == '\x7f' || c > '\xff'
+
+simpleEscapes :: [(Char, String)]
+simpleEscapes = zipWith ch "\b\n\f\r\t\\\"/" "bnfrt\\\"/"
+    where ch a b = (a, ['\\',b])
+
+smallHex :: Int -> Doc
+smallHex x  = text "\\u"
+           <> text (replicate (4 - length h) '0')
+           <> text h
+    where h = showHex x ""
+
+astral :: Int -> Doc
+astral n = smallHex (a + 0xd800) <> smallHex (b + 0xdc00)
+    where a = (n `shiftR` 10) .&. 0x3ff
+          b = n .&. 0x3ff
+
+hexEscape :: Char -> Doc
+hexEscape c | d < 0x10000 = smallHex d
+            | otherwise   = astral (d - 0x10000)
+  where d = ord c
--- ch05/PrettyStub.hs
+++ ch05/PrettyStub.hs
@@ -5,20 +5,17 @@ import SimpleJSON
 data Doc =  ToBeDefined
   deriving (Show)
 
-string :: String -> Doc
-string = enclose '"' '"' . hcat . map oneChar
-
 double :: Double -> Doc
 double num = undefined
 
 text :: String -> Doc
 text str = undefined
 
-enclose :: Char -> Char -> Doc -> Doc
-enclose rightChar leftChar doc = undefined
-
 hcat :: [Doc] -> Doc
 hcat ds = undefined
 
-oneChar :: Char -> Doc
-oneChar x = undefined
+(<>) :: Doc -> Doc -> Doc
+a <> b = undefined
+
+char :: Char -> Doc
+char c = undefined
