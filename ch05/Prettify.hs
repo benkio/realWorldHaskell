97bf7d7505642a47fa3be66707785bfccd9fa9fb
Enrico Benini <ebenini@mdsol.com>
2019-01-08 10:55:32 +0000

    add the pretty implementation

--- ch05/Prettify.hs
+++ ch05/Prettify.hs
@@ -2,11 +2,6 @@ module Prettify where
 
 import Prelude hiding ((<>))
 
-{- STUBS -}
-pretty = undefined
-
-{- END OF STUBS -}
-
 data Doc = Empty
          | Char Char
          | Text String
@@ -73,3 +68,25 @@ compact doc = transform [doc]
             Line -> '\n' : transform ds
             Concat d1 d2 -> transform(d1:d2:ds)
             Union _ d2 -> transform(d2:ds)
+
+fits :: Int -> String -> Bool
+w `fits` _ | w < 0 = False
+_ `fits` ""        = True
+_ `fits` ('\n':_)  = True
+w `fits` (_:cs)    = (w - 1) `fits` cs
+
+pretty :: Int -> Doc -> String
+pretty width x = best 0 [x]
+    where best col (d:ds) =
+              case d of
+                Empty        -> best col ds
+                Char c       -> c :  best (col + 1) ds
+                Text s       -> s ++ best (col + length s) ds
+                Line         -> '\n' : best 0 ds
+                a `Concat` b -> best col (a:b:ds)
+                a `Union` b  -> nicest col (best col (a:ds))
+                                           (best col (b:ds))
+          best _ _ = ""
+          nicest col a b | (width - least) `fits` a = a
+                         | otherwise                = b
+                         where least = min width col
