dda1e3fed1de6610ed6e218b179a7e19f1bb07ff
Enrico Benini <ebenini@mdsol.com>
2019-01-10 17:37:13 +0000

    Chapter 6: More helpful errors

---
+++ ch06/JSONClass.hs
@@ -0,0 +1,11 @@
+import SimpleJSON
+
+type JSONError = String
+
+class JSON a where
+  toJValue :: a -> JValue
+  fromJValue :: JValue -> Either JSONError a
+
+instance JSON JValue where
+    toJValue = id
+    fromJValue = Right
