384fb2efd13c2ce17a7fdf3d91c4e069ac4ccdc9
Enrico Benini <ebenini@mdsol.com>
2019-01-21 17:59:09 +0000

    Chapter 6: Exercises

--- ch06/JSONClass.hs
+++ ch06/JSONClass.hs
@@ -1,10 +1,29 @@
 {-# LANGUAGE FlexibleInstances #-}
 
-module JSONClass where
+module JSONClass(
+  JAry(fromJAry),
+  jary,
+  JObj(fromJObj),
+  jobj,
+  doubleToJValue
+  ) where
 
-import SimpleJSON
+import Data.Either
+import Control.Arrow (second)
 
 type JSONError = String
+newtype JAry a = JAry {
+  fromJAry :: [a]
+  } deriving (Eq, Ord, Show)
+newtype JObj a = JObj { fromJObj :: [(String, a)] } deriving (Ord, Eq, Show)
+
+data JValue = JString String
+            | JNumber Double
+            | JBool Bool
+            | JNull
+            | JObject (JObj JValue)   -- was [(String, JValue)]
+            | JArray (JAry JValue)    -- was [JValue]
+              deriving (Eq, Ord, Show)
 
 class JSON a where
   toJValue :: a -> JValue
@@ -36,6 +55,47 @@ instance JSON Double where
     toJValue = JNumber
     fromJValue = doubleToJValue id
 
+instance JSON a => JSON (JAry a) where
+  toJValue = jaryToJValue
+  fromJValue = jaryFromJValue
+
+instance (JSON a) => JSON (JObj a) where
+    toJValue = JObject . JObj . map (second toJValue) . fromJObj
+
+    fromJValue (JObject (JObj o)) = whenRight JObj (mapEithers unwrap o)
+      where unwrap (k,v) = whenRight ((,) k) (fromJValue v)
+    fromJValue _ = Left "not a JSON object"
+
 doubleToJValue :: (Double -> a) -> JValue -> Either JSONError a
 doubleToJValue f (JNumber n) = Right (f n)
 doubleToJValue _ _ = Left "Not a JSON Number"
+
+jary :: [a] -> JAry a
+jary = JAry
+
+jaryFromJValue :: (JSON a) => JValue -> Either JSONError (JAry a)
+jaryFromJValue (JArray (JAry a)) = whenRight JAry (mapEithers fromJValue a)
+jaryFromJValue _ = Left "Error: Impossible to convert to JAry"
+
+jaryToJValue :: (JSON a) => JAry a -> JValue
+jaryToJValue =  jaryOfJValuesToJValue . jary . listToJValues . fromJAry
+
+jobj :: [(String, a)] -> JObj a
+jobj = JObj
+
+listToJValues :: (JSON a) => [a] -> [JValue]
+listToJValues = map toJValue
+
+jaryOfJValuesToJValue :: JAry JValue -> JValue
+jaryOfJValuesToJValue = JArray
+
+whenRight :: (b -> c) -> Either a b -> Either a c
+whenRight _ (Left x) = Left x
+whenRight f (Right x) = Right (f x)
+
+mapEithers :: (a -> Either b c) -> [a] -> Either b [c]
+mapEithers _ [] = Right []
+mapEithers f (x:xs) = do
+  c <- f x
+  cs <- mapEithers f xs
+  return (c:cs)
