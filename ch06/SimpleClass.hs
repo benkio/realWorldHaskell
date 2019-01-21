80f4c6e98be116ac4d3c133d66ef7cfa2a27e861
Enrico Benini <ebenini@mdsol.com>
2019-01-21 14:52:09 +0000

    Chapter 6: How does show work for strings?

---
+++ ch06/SimpleClass.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
+
+import Data.List
+
+class Foo a where
+  foo :: a -> String
+
+instance Foo a => Foo [a] where
+  foo = concat . intersperse ", " . map foo
+
+instance Foo Char where
+  foo c = [c]
+
+instance {-# OVERLAPPING #-} Foo String where
+  foo = id
+
