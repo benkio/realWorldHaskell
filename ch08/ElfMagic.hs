4f93d19fd1a043ad8539953f3f3fc07bc39a6cad
Enrico Benini <ebenini@mdsol.com>
2019-02-15 11:48:43 +0000

    Chapter 8: Text I/O

---
+++ ch08/ElfMagic.hs
@@ -0,0 +1,11 @@
+
+import qualified Data.ByteString.Lazy as L
+
+hasElfMagic :: L.ByteString -> Bool
+hasElfMagic bs = L.take 4 bs == elfMagic
+  where elfMagic = L.pack [0x7f, 0x45, 0x4c, 0x46]
+
+isEflFile :: FilePath -> Bool
+isEflFile path = do
+  content <- L.readFile path
+  return (hasElfMagic content)
